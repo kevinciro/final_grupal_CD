fed423c33909af40773a3b8308637b7831230b81
kevinciro <70342180+kevinciro@users.noreply.github.com>
2022-09-14 18:00:58 -0500

    Delete final_grupalCristian directory

--- final_grupalCristian/README.md
+++
@@ -1,2 +0,0 @@
-# final_grupal_CD
-Repo para el trabajo final en grupo de Coding Dojo, grupo 5
--- final_grupalCristian/flask_app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-#Generar la secret_key
-app.secret_key = "Esta es mi llave super secreta"
